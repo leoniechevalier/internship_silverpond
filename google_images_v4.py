5ad1d48750bef29bc880bcd6f526d7a0da4924b9
lchevalier <leonie.chevalier.le.ch@gmail.com>
2018-05-21 15:50:40 +1000

    added function to read in single or multiple files and make array of dataframes

--- google_images_v4.py
+++ google_images_v4.py
@@ -8,6 +8,7 @@ It should work best when using pandas as a dataframe structure
 but that can be rewritten.
 '''
 
+
 def find_class (class_name,array,column):
     ''' find a specific class in an array'''
     
@@ -24,4 +25,47 @@ def find_class (class_name,array,column):
     return filtered_array
 
 
-    #Needs section on how to lead in data
+# Needs section on how to lead in data
+# find where data is stored
+
+# read in data    
+test_im = pd.read_csv(path+filename, header=0)
+cl_des = pd.read_csv('class-descriptions-boxable.csv', names=['LabelName','name'])
+
+def read_file(filename, path):
+	filename = np.array(filename)
+	dataframe_name = np.array(dataframe_name)
+	'''reads in data files in various formats 
+	(txt and csv so far, expand as neccecary)'''
+
+	if len(filename)>1:
+		frames = []
+		for i in filename:
+			ending = i[-3:]
+			
+			if ending == 'csv':
+				dataframe = pd.read_csv(path+i)
+				frames.append(dataframe)
+			elif ending == 'txt':
+				dataframe = pd.read_table(path+i)
+				frames.append(dataframe)
+	
+			else :
+				raise NameError('Invalid data type. Please review input or add to script.')
+	
+		return frames
+	else:
+		ending = filename[-3:]
+	
+		if ending == 'csv':
+			dataframe = pd.read_csv(path+filename)
+		elif ending == 'txt':
+			dataframe = pd.read_table(path+filename)
+	
+		else :
+			raise NameError('Invalid data type. Please review input or add to script.')
+	
+		return dataframe
+
+
+
